72849834b3e3f9b0a7d38f3f87a2144454db22ec
maxxxkam <muslimovmaxkam@gmail.com>
2024-08-09 11:23:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let massiv = [ 'apple', 'banana', 'orange']
+
+// push - добавляет в конец массива
+massiv.push('grape')
+
+// unshift - добавяет к начало массива
+massiv.unshift('cherry')
+
+// pop - удаляет последний элемент в массиве
+massiv.pop()
+
+// shift - удаляет первый элемент в массиве
+massiv.shift()
+
+
+let fruits = ['apple', 'banana', 'orange']
+
+let fruits2 = ['mango', 'melon']
+
+// concat объединяет массивы
+let all = fruits.concat(fruits2)
+
+console.log(all);
+
+
+let fruitss = ['apple', 'banana', 'orange']
+
+// преобразует массив в единную строку
+let all1 = fruits.join(', ')
+
+console.log(all);
